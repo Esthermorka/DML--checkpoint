075fea320f8894b628f5c51c906298809f6c817a
Esthermorka <114414746+Esthermorka@users.noreply.github.com>
2022-11-16 10:28:30 +0100

    Update dml checkpoint.sql

--- dml checkpoint.sql
+++ dml checkpoint.sql
@@ -1,35 +1,17 @@
 insert into customer 
-values ( 'C02',
-	'Asma',
-	77345823
-)
+values ( 'C01', 'Ali', 77345823)
+
+insert into customer 
+values ( 'C02','Asma',77345823)
 
 insert into product_1 
-values ('P01',
-		'Samsung Galaxy S20',
-		3299,
-		'SmartPhone'
-)
+values ('P01','Samsung Galaxy S20',3299,'SmartPhone')
 
 insert into product_1 
-values ('P02',
-		'Asus Notebook',
-		4599,
-		'PC'
-)
+values ('P02','Asus Notebook',4599,'PC')
 
 insert into orders (customer_id, product_id,quantity,total_amount)
-values ('C01',
-		'P02',
-		2,
-		9198
-)
+values ('C01','P02',2,9198)
 
 insert into orders (customer_id, product_id,quantity,total_amount,orderdate)
-values ('C02',
-		'P01',
-		1,
-		3299,
-		'2020-05-28'
-)
-select*from product_1
+values ('C02',P01',1,3299,'2020-05-28')
